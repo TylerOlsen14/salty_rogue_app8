5c3e309cd9dce52f5626fc6b27c4173b95d3ce10
TylerOlsen14 <Tyler.Olsen1@gmail.com>
2019-04-30 11:47:32 -0600

    configured keys.js

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI : 'mongodb://Tucker:Tucker@cluster0-shard-00-00-tihhu.mongodb.net:27017,cluster0-shard-00-01-tihhu.mongodb.net:27017,cluster0-shard-00-02-tihhu.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
+}
+// module.exports = {
+//   mongoURI : 'mongodb://Tucker:Tucker@cluster0-shard-00-00-tihhu.mongodb.net:27017,cluster0-shard-00-01-tihhu.mongodb.net:27017,cluster0-shard-00-02-tihhu.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
+// }
