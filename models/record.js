6b41c97cddc5157dd7dd90cb0a14e6711fd7f0eb
TylerOlsen14 <Tyler.Olsen1@gmail.com>
2019-04-30 11:46:34 -0600

    configured models/record.js

---
+++ models/record.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+const RecordSchema = new Schema({
+  ClientName: {
+    type: String,
+    required: false
+  },
+  ClientPhoneNumber: {
+    type: Number,
+    required: false,
+  },
+  ClientNotes: {
+    type: String,
+    required: false
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = Record = mongoose.model('record', RecordSchema);
