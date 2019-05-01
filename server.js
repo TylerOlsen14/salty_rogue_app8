99877aeaab8f2e4d359eb838476ad648a457070b
TylerOlsen14 <Tyler.Olsen1@gmail.com>
2019-05-01 14:19:15 -0600

    investigating how to pull address data

--- server.js
+++ server.js
@@ -11,12 +11,13 @@ app.use(cors())
 //Bodyparser middleware
 app.use(bodyParser.json());
 
-// // DB config
-// const db = require('./config/keys').mongoURI;
+// DB config
+const db = require('./config/keys').mongoURI;
 
 //connect to Mongo
 mongoose
-  .connect("mongodb://Tucker:Tucker@cluster0-shard-00-00-tihhu.mongodb.net:27017,cluster0-shard-00-01-tihhu.mongodb.net:27017,cluster0-shard-00-02-tihhu.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true", { useNewUrlParser: true })
+  .connect(db, { useNewUrlParser: true })
+  // .connect("mongodb://Tucker:Tucker@cluster0-shard-00-00-tihhu.mongodb.net:27017,cluster0-shard-00-01-tihhu.mongodb.net:27017,cluster0-shard-00-02-tihhu.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true", { useNewUrlParser: true })
   .then(() => console.log('MongoDB connected ... '))
   .catch(err => console.log(err));
 
